14fc91865466ecdf319db3c4151ee8415bfe092c
A00306078 <A00306078@mycambrian.ca>
2024-12-14 03:22:00 +0000

    Nuget packege created

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class HelloWorld
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello from MyLibrary!");
+        }
+    }
+}
